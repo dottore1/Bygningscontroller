2ef499924b72968caee377ca68b4bde8194b67ae
Markus Munk <mmunk19@student.sdu.dk>
2019-11-20 14:32:12 +0100

    implementation of interface in BuildingManager
    dab

--- bygningsAdministrationGUI/src/main/java/domain/Building.java
+++ bygningsAdministrationGUI/src/main/java/domain/Building.java
@@ -3,35 +3,75 @@ package domain;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Building extends Unit {
+public class Room extends Unit {
 
-    private ArrayList<Room> roomList;
+    private ArrayList<Aktuator> aktuatorList;
+    private ArrayList<Sensor> sensorList;
 
-    public Building(String name) {
+    public Room(String name) {
         super(name);
-        roomList = new ArrayList<>();
+        aktuatorList = new ArrayList<>();
+        sensorList = new ArrayList<>();
     }
-    
-        public void addRoom(Room room) {
-            this.roomList.add(room);
-            
+
+    public ArrayList<Aktuator> getAktuatorList() {
+        return aktuatorList;
     }
-        
-            public void removeRoom(UUID id) {
-        for (Room room : roomList) {
-            if (room.getId().equals(id)) {
-                roomList.remove(room);
-                if (roomList.remove(room) == false) {
+
+    public ArrayList<Sensor> getSensorList() {
+        return sensorList;
+    }
+
+    public void addAktuator(Aktuator aktuator) {
+        if (aktuator instanceof TempatureAktuator) {
+            this.aktuatorList.add(aktuator);
+
+        } else if (aktuator instanceof VentilatorAktuator) {
+            this.aktuatorList.add(aktuator);
+        } else {
+            System.out.println("This is not a valid Aktuator..");
+        }
+
+    }
+
+    public void addSensor(Sensor sensor) {
+        if (sensor instanceof TempatureSensor) {
+            this.sensorList.add(sensor);
+
+        } else if (sensor instanceof Co2LevelSensor) {
+            this.sensorList.add(sensor);
+        } else {
+            System.out.println("This is not a valid sensor..");
+        }
+
+    }
+
+    public void removeAktuator(UUID id) {
+        for (Aktuator aktuator : aktuatorList) {
+            if (aktuator.getId().equals(id)) {
+                aktuatorList.remove(aktuator);
+                if (aktuatorList.remove(aktuator) == false) {
                     System.out.println("removal unsuccesfull.");
                 }
             }
         }
     }
-
-    @Override
-    public String toString() {
-        return "Building: " + super.getName();
-    }
     
-}
+        public void removeSensor(UUID id) {
+        for (Sensor sensor : sensorList) {
+            if (sensor.getId().equals(id)) {
+                sensorList.remove(sensor);
+                if (sensorList.remove(sensor) == false) {
+                    System.out.println("removal unsuccesfull.");
+                }
+            }
+        }
+    }
+        
+        @Override
+        public String toString(){
+            String txt = "Room: " + super.getName();
+            return txt;
+        }
 
+}
